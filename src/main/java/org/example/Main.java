8f250621d9aa99d7c5335a20af2c631763911b81
DmytroAbramiuk <128092807+DmytroAbramiuk@users.noreply.github.com>
2024-01-05 19:03:31 +0200

    added line in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println(SumCalculator.sum(1, 2));
+        System.out.println(SumCalculator.sum(5,6));
     }
 }
