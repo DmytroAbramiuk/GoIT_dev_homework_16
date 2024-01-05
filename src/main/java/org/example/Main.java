d6e53ef8830f03bc22c7058098834c61b37e144a
DmytroAbramiuk <128092807+DmytroAbramiuk@users.noreply.github.com>
2024-01-05 19:08:40 +0200

    removed line in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println(SumCalculator.sum(1, 2));
-        System.out.println(SumCalculator.sum(5,6));
-        System.out.println("Hello");
     }
 }
